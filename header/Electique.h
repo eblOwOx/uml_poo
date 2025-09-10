42378521f709d850c961a9e5ee843a9ed340bf10
eblOwOx <amaurymplom@proton.me>
2025-09-10 16:58:58 +0200

    ajout de Electrique.h

---
+++ header/Electique.h
@@ -0,0 +1,34 @@
+/*****************************************************************//**
+* \file   Electrique.h
+* \brief  Declaration de la classe Electrique
+*
+* \author Amaury
+* \date   September 2025
+*********************************************************************/
+
+#define ELECTRIC_H
+#include <iostream>
+#include <string>
+
+/*! \class Electrique
+ * \brief classe representant un moteur electrique. 1 attributs prives
+ */
+class Electrique
+{
+private:
+    /*Classe sous forme canonique*/
+    float tensionMax;
+
+protected:
+
+public:
+    /*Classe sous forme canonique*/
+    Electrique();
+    ~Electrique();
+    Electique(const Electrique& elec);
+    Electrique &operator = (const Electrique& elec);
+
+    Electrique(float tensionMax);
+    void setTensionMax(float puissance);
+    float getTentionMax();
+};
