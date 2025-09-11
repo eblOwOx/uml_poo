5aebb32bd38bbdb2d41f2cd9098d086bee3591e3
eblOwOx <amaurymplom@proton.me>
2025-09-11 09:19:55 +0200

    test de la classe d'objet moto mit en place

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+/*
+ * Point d'entrée du programme
+ * Permet de tester les différentes classes d'objets
+ * NE PAS C
+ * */
 #include <iostream>
 #include "source/Moto.cpp"
+//#include "source/Electrique.cpp"
 
 int main () {
-  std::cout << "test";
+  Moto moto1;
+  moto1.setPoids(50);
+
+  std::cout << "Poids de la moto : " << moto1.getPoids() << "." << endl;
   return 0;
 }
